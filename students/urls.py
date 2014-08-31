8c211b7221336fdc707890f04b8e387dd7028ed4
Vadim Velikodniy <vadim@velikodniy.name>
2014-08-31 18:32:17 +0300

    Добавлена форма удаления студента

--- students/urls.py
+++ students/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
-from students.views import StudentUpdate, StudentCreate
+from students.views import StudentUpdate, StudentCreate, StudentDelete
 
 urlpatterns = patterns('',
                        url(r'^$', 'students.views.index',
@@ -11,6 +11,8 @@ urlpatterns = patterns('',
                            name='students-edit'),
                        url(r'create', StudentCreate.as_view(),
                            name='students-create'),
+                       url(r'^(?P<pk>\d+)-delete$', StudentDelete.as_view(),
+                           name='students-delete'),
                        url(r'^group-(?P<group_id>\d+)$', 'students.views.group_detail',
                            name='students-group-detail'),
 )
