f4886e089757498c35be547debffc71efa55bdb2
Vadim Velikodniy <vadim@velikodniy.name>
2014-09-04 17:55:56 +0300

    Просмотр списка семинаров

--- seminars/urls.py
+++ seminars/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, url
 
-from seminars.views import SeminarDetail
+from seminars.views import SeminarListView, SeminarDetail
 
 urlpatterns = \
     patterns('',
-             # url(r'^$', 'seminars.views.index', name='seminars-index'),
+             url(r'^$', SeminarListView.as_view(), name='seminars-index'),
              url(r'^(?P<seminar_id>\d+)$', SeminarDetail.as_view(), name='seminars-detail'),
              # url(r'^(?P<pk>\d+)-edit$', SeminarUpdate.as_view(), name='seminars-edit'),
              # url(r'^create$', SeminarCreate.as_view(), name='seminars-create'),
--- seminars/views.py
+++ seminars/views.py
@@ -1,8 +1,14 @@
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 from seminars.models import Seminar, SeminarMaterials
 
+
 # Семинар =======================================
+class SeminarListView(ListView):
+    model = Seminar
+
+
 class SeminarDetail(DetailView):
     model = Seminar
     pk_url_kwarg = 'seminar_id'
