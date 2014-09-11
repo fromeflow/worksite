604d9115f63322927fb9f9ae4dcd9a52be64906c
Vadim Velikodniy <vadim@velikodniy.name>
2014-09-11 22:17:51 +0300

    Добавлена админка для учебных пособий

--- textbooks/admin.py
+++ textbooks/admin.py
@@ -1,1 +1,24 @@
-# Register your models here.
+from django.contrib import admin
+
+from textbooks.models import Author, Textbook, TextbookMaterial
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['surname', 'user']
+
+
+admin.site.register(Author, AuthorAdmin)
+
+
+class TextbookAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+admin.site.register(Textbook, TextbookAdmin)
+
+
+class TextbookMaterialAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+admin.site.register(TextbookMaterial, TextbookMaterialAdmin)
