2e942cc386f3d5cb2c0a321720f08b0dada23705
Vadim Velikodniy <vadim@velikodniy.name>
2014-09-30 00:41:02 +0300

    Добавлен тег для множественного числа

--- misc/templatetags/text.py
+++ misc/templatetags/text.py
@@ -12,3 +12,17 @@ def mark_to_text(value):
         5: 'отлично'
     }
     return MARKS.get(value, str(value))
+
+
+@register.filter(name='plural_ru')
+def mark_to_text(value, arg):
+    name1, name2, name5, *_ = arg.split(',')
+    value_int = int(value)
+    if (value_int // 10 % 10) == 1:
+        return name5
+    elif 2 <= (value_int % 10) <= 4:
+        return name2
+    elif value_int % 10 == 1:
+        return name1
+    else:
+        return name5
