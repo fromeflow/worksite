9d733191ecc13dab957c391c7b9a9f71c2da0aeb
Vadim Velikodniy <vadim@velikodniy.name>
2014-10-02 16:35:22 +0300

    Модели форума зарегистрированы в админке

--- forum/admin.py
+++ forum/admin.py
@@ -1,1 +1,17 @@
-# Register your models here.
+from django.contrib import admin
+
+from forum.models import Theme, Message
+
+
+class ThemeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Theme, ThemeAdmin)
+
+
+class MessageAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Message, MessageAdmin)
