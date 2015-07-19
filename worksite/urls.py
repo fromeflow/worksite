a86622df294477e1bcf510d6bd55e13fffba2d33
Vadim Velikodniy <vadim@velikodniy.name>
2015-07-19 09:22:13 +0300

    Заглушка заглавной страницы

--- worksite/urls.py
+++ worksite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.shortcuts import render
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', lambda r: render(r, 'index.html'))
 ]
