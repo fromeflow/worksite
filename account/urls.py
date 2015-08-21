cb40b475de0d1f607b6804dfbc55e9652634d868
Vadim Velikodniy <vadim@velikodniy.name>
2015-08-21 18:33:15 +0300

    Исправлена ошибка с url для выхода

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 import django.contrib.auth.views as auth_views
+from django.core.urlresolvers import reverse_lazy
 
 urlpatterns = patterns('',
                        url(r'^login$', auth_views.login, {'template_name': 'account/login.html'},
                            name='login'),
-                       url(r'^logout$', 'account.views.logout_view',
+                       url(r'^logout$', auth_views.logout_then_login, {'login_url': reverse_lazy('account:login')},
                            name='logout'),
 )
