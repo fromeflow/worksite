a92a420f6e7a587fcee59d02da9fa2ce67d07669
Vadim Velikodniy <vadim@velikodniy.name>
2015-08-28 18:47:08 +0300

    Библиотечка с типичными валидаторами моделей

---
+++ utils/validators.py
@@ -0,0 +1,5 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+year_validator = [MinValueValidator(1930), MaxValueValidator(2100)]
+mark5_validator = [MinValueValidator(2), MaxValueValidator(5)]
+level_validator = [MinValueValidator(1), MaxValueValidator(7)]
