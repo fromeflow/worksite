0435eb824546cf9b2fe0ae249e3c29a4466f790b
Vadim Velikodniy <vadim@velikodniy.name>
2015-09-12 21:38:42 +0300

    Заготовка представлений для отображения списков студенческих проектов

--- projects/urls.py
+++ projects/urls.py
@@ -5,16 +5,17 @@ from projects.views import *
 
 urlpatterns = \
     patterns('',
-             # url(r'^$', CourseProjectList.as_view(), name='index'),
-             # url(r'final^$', FinalProjectList.as_view(), name='final-index'),
-             # url(r'open^$', OpenProjectList.as_view(), name='open-index'),
-             # url(r'closed^$', OpenProjectList.as_view(), name='closed-index'),
-             #
-             # url(r'^create$', CourseProjectCreate.as_view(), name='create'),
-             # url(r'^final-create$', FinalProjectCreate.as_view(), name='final-create'),
+             url(r'^$', CourseProjectList.as_view(), name='index'),
+             url(r'^open$', OpenProjectList.as_view(), name='open-index'),
+             url(r'^final$', FinalProjectList.as_view(), name='final-index'),
+             url(r'^open-final$', OpenFinalProjectList.as_view(), name='open-final-index'),
 
              url(r'^(?P<pk>\d+)$', CourseProjectDetail.as_view(), name='detail'),
              url(r'^final-(?P<pk>\d+)$', FinalProjectDetail.as_view(), name='final-detail'),
+
+             # url(r'^create$', CourseProjectCreate.as_view(), name='create'),
+             # url(r'^final-create$', FinalProjectCreate.as_view(), name='final-create'),
+
              # url(r'^(?P<pk>\d+)-edit$', ProjectUpdate.as_view(), name='edit'),
              # url(r'^(?P<pk>\d+)-delete$', ProjectDelete.as_view(), name='delete'),
     )
--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 from .models import CourseProject, FinalProject
 
+
+# Списки
+class CourseProjectList(ListView):
+    queryset = CourseProject.objects\
+        .select_related('student')\
+        .all()
+
+class OpenProjectList(ListView):
+    template_name_suffix = '_open_list'
+    queryset = CourseProject.objects\
+        .filter(student=None)\
+        .all()
+
+class FinalProjectList(ListView):
+    queryset = FinalProject.objects\
+        .select_related('student')\
+        .all()
+
+class OpenFinalProjectList(ListView):
+    template_name_suffix = '_open_list'
+    queryset = FinalProject.objects\
+        .filter(student=None)\
+        .all()
+
+# Подробная информация
 class CourseProjectDetail(DetailView):
     model = CourseProject
 
