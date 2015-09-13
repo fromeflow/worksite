881260bcceedb15658ebc03dfa7cc1923544baa9
Vadim Velikodniy <vadim@velikodniy.name>
2015-09-13 11:15:24 +0300

    Поддержка фильтра markdown

--- utils/templatetags/html.py
+++ utils/templatetags/html.py
@@ -1,5 +1,8 @@
+from markdown import markdown as md
+
 from django import template
 from django.utils.safestring import mark_safe
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
@@ -12,3 +15,8 @@ def link_icon(object):
 @register.filter
 def link(object):
     return mark_safe(object.to_link())
+
+@register.filter
+@stringfilter
+def markdown(value):
+    return mark_safe(md(value, output_format='html5', safe_mode='escape'))
