c7592d8501856bc6284c23fdc038e888310e378a
Vadim Velikodniy <vadim@velikodniy.name>
2015-09-15 09:15:55 +0300

    Заготовка подключения классов из courses к админке

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import \
+    Course, CourseVersion, CourseSemester, \
+    Exam, ExamMark, Lecture
+
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Course, CourseAdmin)
+
+
+class CourseVersionAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(CourseVersion, CourseVersionAdmin)
+
+
+class CourseSemesterAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(CourseSemester, CourseSemesterAdmin)
+
+
+class ExamAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Exam, ExamAdmin)
+
+
+class ExamMarkAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(ExamMark, ExamMarkAdmin)
+
+
+class LectureAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Lecture, LectureAdmin)
