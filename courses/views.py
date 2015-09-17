9afdae15c8655c8b690af360af085cda863d1161
Vadim Velikodniy <vadim@velikodniy.name>
2015-09-17 20:28:55 +0300

    Замена DetailView на TemplateView

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
+from django.views.generic.base import TemplateView
 from django.db.models import Max
 from django.http import Http404
 
@@ -12,25 +12,24 @@ class CourseList(ListView):
         .filter(closed=False)\
         .all()
 
-class CourseLastVersionDetail(DetailView):
+class CourseLastVersionDetail(TemplateView):
     template_name = 'courses/courseversion_detail.html'
-    def get_object(self):
-        max_version = CourseVersion.objects.filter(course=self.kwargs['pk'])\
-            .aggregate(Max('version'))['version__max']
-        try:
-            last_course_version = CourseVersion.objects\
-                .get(version=max_version)
-        except CourseVersion.DoesNotExist:
-            return None
-        return last_course_version
 
     def get_context_data(self, **kwargs):
         context = super(CourseLastVersionDetail, self).get_context_data(**kwargs)
+
+        max_version = CourseVersion.objects.filter(course=self.kwargs['pk'])\
+            .aggregate(Max('version'))['version__max']
+        try:
+            context['courseversion'] = CourseVersion.objects\
+                .get(version=max_version)
+        except CourseVersion.DoesNotExist:
+            context['courseversion'] = None
+
         try:
             context['course'] = Course.objects.\
                 select_related('specialty', 'specialty__chair').\
                 get(id=self.kwargs['pk'])
         except Course.DoesNotExist:
             raise Http404()
-        context['courseversion'] = context['object']
         return context
