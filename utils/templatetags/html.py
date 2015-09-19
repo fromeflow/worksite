8a84fdccdbb1aa31b4f1de95bd75b571ef1e964a
Vadim Velikodniy <vadim@velikodniy.name>
2015-09-19 19:23:58 +0300

    Подсветка синтаксиса и таблицы в markdown

--- utils/templatetags/html.py
+++ utils/templatetags/html.py
@@ -1,5 +1,8 @@
 from markdown import markdown as md
 from markdown.extensions.toc import TocExtension
+from markdown.extensions.codehilite import CodeHiliteExtension
+from markdown.extensions.fenced_code import FencedCodeExtension
+from markdown.extensions.tables import TableExtension
 
 from django import template
 from django.utils.safestring import mark_safe
@@ -22,5 +25,11 @@ def link(object):
 def markdown(value, header_level=1):
     html = md(value,
               output_format='html5', safe_mode='escape',
-              extensions=[TocExtension(baselevel=header_level)])
+              extensions=[
+                  TocExtension(baselevel=header_level),
+                  CodeHiliteExtension(),
+                  FencedCodeExtension(),
+                  TableExtension(),
+              ])
     return mark_safe(html)
+
