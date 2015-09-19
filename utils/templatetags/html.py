132b166cea34972bf67a0d7ece81bf7c113b95bc
Vadim Velikodniy <vadim@velikodniy.name>
2015-09-19 17:49:43 +0300

    Параметр фильтра markdown — базовый уровень заголовков

--- utils/templatetags/html.py
+++ utils/templatetags/html.py
@@ -1,4 +1,5 @@
 from markdown import markdown as md
+from markdown.extensions.toc import TocExtension
 
 from django import template
 from django.utils.safestring import mark_safe
@@ -18,5 +19,8 @@ def link(object):
 
 @register.filter
 @stringfilter
-def markdown(value):
-    return mark_safe(md(value, output_format='html5', safe_mode='escape'))
+def markdown(value, header_level=1):
+    html = md(value,
+              output_format='html5', safe_mode='escape',
+              extensions=[TocExtension(baselevel=header_level)])
+    return mark_safe(html)
